83f1988128d614fcaf55ded4c5a78e6158d34295
Joey Li <joeyzaozaoli@gmail.com>
2017-07-27 10:48:45 -0700

    Create and render nested React components

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-// TODO
+var GroceryList = () => (
+  <ul>
+    <Salad />
+    <IceCream />
+  </ul>
+)
+
+var Salad = () => (
+  <li>Seaweed Salad</li>
+)
+
+var IceCream = () => (
+  <li>Coconut Ice Cream</li>
+)
+
+ReactDOM.render(<GroceryList />, document.getElementById('app'));
